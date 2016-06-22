f1a6d78423b4575bf3b074d1737d86d73b3a174b
Rubens L. V. Jr <rubens.vieira.junior@gmail.com>
2016-06-22 09:55:00 -0300

    InstrumentFactory

--- src/com/rlvj/structural/flyweight/InstrumentFactory.java
+++ src/com/rlvj/structural/flyweight/InstrumentFactory.java
@@ -1,5 +1,27 @@
 package com.rlvj.structural.flyweight;
 
+import java.util.HashMap;
+
 public class InstrumentFactory {
 
+	private static final HashMap<InstrumentType, Instrument> instruments = new HashMap<InstrumentType, Instrument>();
+	
+	public static Instrument getInstrument(InstrumentType type) {
+		Instrument instrument = instruments.get(type);
+		if (instrument == null) {
+			if (type.equals(InstrumentType.STRINGS)) {
+				instrument = new Guitar();
+			} else if (type.equals(InstrumentType.STINGS)) {
+				instrument = new Piano(false);
+			} else if (type.equals(InstrumentType.STINGS_TWICE)) {
+				instrument = new Piano(true);
+			}
+		}
+		
+		return instrument;
+	}
+	
+	public static enum InstrumentType {
+		STRINGS, STINGS, STINGS_TWICE
+	}
 }
